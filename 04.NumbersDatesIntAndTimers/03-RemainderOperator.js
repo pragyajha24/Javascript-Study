e9ec72f527a962e12ef8862434b8ff14ec965ddf
Pragya  Jha <pragyajha50@gmail.com>
2024-10-04 16:13:54 +0530

    Create 03-RemainderOperator.js

---
+++ 04.NumbersDatesIntAndTimers/03-RemainderOperator.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const diameter = 287_460_000_000;
+console.log(diameter);//287460000000
+
+//improves readability
+const price = 763_99;
+console.log(price);//76399
+
+//it ignores the underscore, which means we can use place underscores anywhere in our code to make it more readable withour changing the output.
+
+//not in beginning of a number
+//not in end of a number
+//not put two in a row
+//only after number not symbols
